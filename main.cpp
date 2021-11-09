d7ad48f9e59e82810ffbe03ecf61e70bfecded92
Omkar Pethe <65779904+har-byte@users.noreply.github.com>
2021-11-09 11:27:58 +0000

    ..

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <stdio.h>
+using namespace std;
 
 int main() {
-  
+  int A[5];
+  A[0] = 11;
+  A[1] = 15;
+  A[2] = 20;
+  cout << sizeof(A) <<endl;
+  printf("%d" , A[1]);
+  for(int x:A){
+    cout << x;
+  }
+
+
+
+
 
 } 
